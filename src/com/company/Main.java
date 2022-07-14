53e9a8f8a339f13ef58308f3d6dfdea2c9c2fbdb
sabaltabaev <sardar.rmfc@gmail.com>
2022-07-14 16:01:42 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Triangle triangle = new Triangle();
+        triangle.a = 3;
+        triangle.b = 6;
+        triangle.c = 7;
+        triangle.area();
+
+    }
+}
---
+++ src/com/company/Triangle.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Triangle {
+        int a;
+        int b;
+        int c;
+
+        void area () {
+            System.out.println("The result is: " + a * b * c / 2);
+        }
+
+
+    }
